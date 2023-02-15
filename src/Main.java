f70bd7c7e4df97647c6acefce8022b0de0f7db76
Alice Nguyen <nguye4hn@mail.uc.edu>
2023-02-15 16:18:41 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        final int AGE_REQUIRE = 21;
+        int age = 10;
+
+        if (age >= AGE_REQUIRE){
+            System.out.println("Here is your wrist band, have fun!");
+        }
+        }
+    }
